546b059404c9aef9320453efc12faa63f681078f
mgucma <marek.gucma@gmail.com>
2024-08-20 10:47:37 +0200

    Spring Boot Portfolio Project — Part 3

--- src/main/java/com/marek/onlinebookstore/mapper/BooksMapper.java
+++ src/main/java/com/marek/onlinebookstore/mapper/BooksMapper.java
@@ -3,12 +3,13 @@ package com.marek.onlinebookstore.mapper;
 import com.marek.onlinebookstore.config.MapperConfig;
 import com.marek.onlinebookstore.dto.BookDto;
 import com.marek.onlinebookstore.dto.CreateBookRequestDto;
-import com.marek.onlinebookstore.entity.Book;
+import com.marek.onlinebookstore.model.Book;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface BooksMapper {
+    Book toModel(CreateBookRequestDto requestDto);
+
     BookDto toBookDto(Book book);
 
-    Book toEntity(CreateBookRequestDto createBookRequestDto);
 }
