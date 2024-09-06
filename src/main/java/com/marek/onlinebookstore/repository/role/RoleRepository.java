32ae0773518336bae84f677b96562a77d4575c9e
mgucma <marek.gucma@gmail.com>
2024-09-06 19:46:34 +0200

    Spring Boot Portfolio Project — Part 8 (#11)
    * Spring Boot Portfolio Project — Part 8
    
    * Update src/main/java/com/marek/onlinebookstore/mapper/BooksMapper.java
    
    Co-authored-by: Mateusz Rostek <140905715+MateuszRostek@users.noreply.github.com>
    
    * Update src/main/java/com/marek/onlinebookstore/service/user/UserServiceImpl.java
    
    Co-authored-by: Mateusz Rostek <140905715+MateuszRostek@users.noreply.github.com>
    
    * fix comment
    
    ---------
    
    Co-authored-by: Mateusz Rostek <140905715+MateuszRostek@users.noreply.github.com>

--- src/main/java/com/marek/onlinebookstore/repository/role/RoleRepository.java
+++ src/main/java/com/marek/onlinebookstore/repository/role/RoleRepository.java
@@ -1,7 +1,10 @@
 package com.marek.onlinebookstore.repository.role;
 
 import com.marek.onlinebookstore.model.Role;
+import com.marek.onlinebookstore.model.RoleName;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName name);
 }
