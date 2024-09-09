39ebe3fb32d3f6f805bd2719236dec08fd1d200f
mgucma <marek.gucma@gmail.com>
2024-09-09 20:30:54 +0200

    Add Docker to the project (#17)

--- src/test/java/com/marek/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/marek/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -1,13 +1,8 @@
 package com.marek.onlinebookstore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineBookStoreApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
