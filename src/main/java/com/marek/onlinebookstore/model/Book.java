1ad96cea1d65efb899e381bfe83fac2c9c951b04
mgucma <marek.gucma@gmail.com>
2024-09-27 20:20:03 +0200

    AWS publish 1

--- src/main/java/com/marek/onlinebookstore/model/Book.java
+++ src/main/java/com/marek/onlinebookstore/model/Book.java
@@ -1,6 +1,15 @@
 package com.marek.onlinebookstore.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,7 +18,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.hibernate.type.NumericBooleanConverter;
 
 @Entity
 @Getter
@@ -32,8 +40,7 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-    @Column(name = "is_deleted", nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    @Convert(converter = NumericBooleanConverter.class)
+    @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
